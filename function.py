c80eeba6c806536418e11b1b087e5dea85fdf70a
ZohairZaidi <zohair.zaidi@mail.utoronto.ca>
2023-07-14 23:22:05 -0400

    updates

--- function.py
+++ function.py
@@ -1,7 +1,14 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
 
 def get_image_caption(image_path):
+    
+    image = Image.open(image_path).comvert('RGB')
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = "cuda"
 
+    processor = BlipProcessor.from_pretrained(model_name).to(device)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
 
     pass
 
